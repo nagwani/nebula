27704f55dd6ab2cc938e6c8850eb562b9d3744a3
Balint Kleri <b@balintbrews.com>
2025-12-03 15:12:40 +0100

    chore(component:spacer): remove old comments

--- src/components/spacer/index.jsx
+++ src/components/spacer/index.jsx
@@ -1,18 +1,3 @@
-// Use React/Preact (https://preactjs.com) and Tailwind CSS 4
-// (https://tailwindcss.com).
-// Global CSS is added to all pages with a @theme directive.
-// Tailwind theme variables must be added in Global CSS.
-// @see https://tailwindcss.com/docs/theme.
-// Do not include @import "tailwindcss"!
-
-// Available third party packages:
-// import { clsx } from 'clsx'
-// import { cva } from 'class-variance-authority'
-// import { twMerge } from 'tailwind-merge'
-
-// Import your other code components to use within this component:
-// import Heading from '@/components/my_heading'
-
 import { cva } from "class-variance-authority";
 
 const spacerVariants = cva("", {
